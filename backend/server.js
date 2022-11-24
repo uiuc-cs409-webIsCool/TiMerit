a3569ec669949c60ece90cdec79934e505592883
Zhicheng Tang <46953357+Aaronlalala@users.noreply.github.com>
2022-11-24 15:03:15 -0600

    environment setting
    Rename frontend folder. Install redux and reduxjs/toolkit. Install some backend packages.

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 const express = require("express");
-const app = express();
+const mongoose = require("mongoose");
+const morgan = require("morgan");
 const path = require("path");
+
+const app = express();
 const PORT = process.env.PORT || 8080;
 
 // public folder is used to store static resources
