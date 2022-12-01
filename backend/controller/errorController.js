527a46e1f379b3dcd51342b51360668af826251e
YouyouZhang <jennzhangyy@gmail.com>
2022-11-30 22:10:36 -0600

    finish GET, POST, DELETEbyID for task route

--- backend/controller/errorController.js
+++ backend/controller/errorController.js
@@ -1,6 +1,7 @@
 module.exports = (err, req, res, next) => {
     try{
-        console.log("ERROR MIDDLEWARE: " + err.code);
+        console.log("ERROR MIDDLEWARE: " + err.message);
+        res.status(500).send(err.message);
     }catch(err){
         res.status(500).send("Unknown error occurred.");
     }
