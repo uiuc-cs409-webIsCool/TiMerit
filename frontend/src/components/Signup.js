d7c749d8e2ef1de5e70a5f7b8b7bf40196404412
Zhicheng Tang <46953357+Aaronlalala@users.noreply.github.com>
2022-12-01 14:40:07 -0600

    Sign Up Page Layout

--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import "bootstrap/dist/css/bootstrap.min.css"
-import {Button, Form, Row, Col} from 'react-bootstrap'
+import {Form, Row, Col} from 'react-bootstrap'
 import styles from "./Signup.module.css";
 
 
@@ -9,27 +9,23 @@ function Signup() {
     return (
         <div className={styles.container}>
             <div className={styles.signupForm}>
-                <Row><h3>Sign up for better experiences</h3></Row>
+                <Row className={styles.title}><h3>Sign up for better experiences</h3></Row>
 
-                <Row>
-                    <Col>
-                        <Form.Control type="text" placeholder="First name" ></Form.Control>
-                    </Col>
-                    <Col>
-                        <Form.Control type="text" placeholder="Last name" ></Form.Control>
-                    </Col>
+                <Row className={styles.name_row}>
+                    <Form.Control type="text" placeholder="First name" className={styles.name_input}></Form.Control>
+                    <Form.Control type="text" placeholder="Last name" className={styles.name_input}></Form.Control>
                 </Row>
 
                 <Row>
-                    <Form.Control type="text" placeholder="Email address"></Form.Control>
+                    <Form.Control type="text" placeholder="Email address" className={styles.email_input}></Form.Control>
                 </Row>
 
                 <Row>
-                <Form.Control type="password" placeholder="Password"></Form.Control>
+                <Form.Control type="password" placeholder="Password" className={styles.email_input}></Form.Control>
                 </Row>
 
                 <Row>
-                    <button>Register</button>
+                    <button className={styles.signup_btn}>Register</button>
                 </Row>
 
             </div>
