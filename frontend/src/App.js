9937036c812657fdae40532b06e303cc5bf5be11
YouyouZhang <jennzhangyy@gmail.com>
2022-12-02 22:48:05 -0600

    add frontend demo for COLLECTION schema POST api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,15 @@ import { Link, Routes, Route } from 'react-router-dom';
 
 import Welcome from "./components/Welcome";
 import Signup from "./components/Signup";
+import Collection from "./components/CollectionView";
 
 
 function App() {
     return (
         <div>
             <Routes>
-                <Route path="/" element={<Welcome />} />
+                <Route path="/" element={<Collection />} />
+                {/* <Route path="/" element={<Welcome />} /> */}
                 <Route path="/signup" element={<Signup />} />
             </Routes>
         </div>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,8 @@ import { BrowserRouter } from 'react-router-dom';
 
 import App from "./App"
 
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <BrowserRouter>
