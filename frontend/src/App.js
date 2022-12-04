71318eb3f63104c00c78e363ee2caf8cc2006bf6
YouyouZhang <jennzhangyy@gmail.com>
2022-12-03 23:40:49 -0600

    added basic layout for homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,14 +4,15 @@ import { Link, Routes, Route } from 'react-router-dom';
 import Welcome from "./components/Welcome";
 import Signup from "./components/Signup";
 import Collection from "./components/CollectionView";
-
+import Home from "./components/HomeView";
 
 function App() {
     return (
         <div>
             <Routes>
-                <Route path="/" element={<Collection />} />
-                {/* <Route path="/" element={<Welcome />} /> */}
+                {/* <Route path="/" element={<Collection />} /> */}
+                <Route path="/" element={<Welcome />} />
+                <Route path="/home" element={<Home />} />
                 <Route path="/signup" element={<Signup />} />
             </Routes>
         </div>
